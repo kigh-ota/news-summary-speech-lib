bc3bd00351f393ac7b4838b2e3fe080cdd990ff6
Kaiichiro <kigh.ota@gmail.com>
2024-11-18 00:00:17 +0900

    Summary is composed of paragraphs with uris

--- src/main/java/nss/Summary.java
+++ src/main/java/nss/Summary.java
@@ -1,5 +1,10 @@
 package nss;
 
 import java.util.Date;
+import java.util.List;
 
-public record Summary(String content, Date createdDate, Date oldestNewsDate, Category category) {}
+public record Summary(Content content, Date createdDate, Date oldestNewsDate, Category category) {
+  public record Content(List<Paragraph> paragraphs) {
+    public record Paragraph(String content, List<String> uris) {}
+  }
+}
