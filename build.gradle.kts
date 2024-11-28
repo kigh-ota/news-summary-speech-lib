2a80e8c01666e0bbeb9b871009f3651771b0c250
Kaiichiro <kigh.ota@gmail.com>
2024-11-28 19:49:24 +0900

    include number of news in summary

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "news-summary-speech"
-version = "0.0.8"
+version = "0.0.10"
 
 publishing {
     publications {
--- src/main/java/nss/Summary.java
+++ src/main/java/nss/Summary.java
@@ -3,7 +3,8 @@ package nss;
 import java.util.Date;
 import java.util.List;
 
-public record Summary(Content content, Date createdDate, Date oldestNewsDate, Category category) {
+public record Summary(
+    Content content, Date createdDate, Date oldestNewsDate, Category category, int newsCount) {
   public record Content(List<Paragraph> paragraphs) {
     public record Paragraph(String content, List<String> uris) {}
   }
