56ecb344185048e57b749882f1c734d97f585c8a
Kaiichiro Ota <kigh.ota@gmail.com>
2025-09-01 00:49:26 +0900

    0.0.11

--- src/main/java/nss/Summary.java
+++ src/main/java/nss/Summary.java
@@ -4,7 +4,12 @@ import java.util.Date;
 import java.util.List;
 
 public record Summary(
-    Content content, Date createdDate, Date oldestNewsDate, Category category, int newsCount, String generatedWith) {
+    Content content,
+    Date createdDate,
+    Date oldestNewsDate,
+    Category category,
+    int newsCount,
+    String generatedWith) {
   public record Content(List<Paragraph> paragraphs) {
     public record Paragraph(String content, List<String> uris) {}
   }
