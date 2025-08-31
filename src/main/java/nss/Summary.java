c334f5fc1f5050e82db2c7de5a07909d2c48bcfe
Kaiichiro Ota <kigh.ota@gmail.com>
2025-09-01 00:41:57 +0900

    Add generatedWith field to Summary record

--- src/main/java/nss/Summary.java
+++ src/main/java/nss/Summary.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 public record Summary(
-    Content content, Date createdDate, Date oldestNewsDate, Category category, int newsCount) {
+    Content content, Date createdDate, Date oldestNewsDate, Category category, int newsCount, String generatedWith) {
   public record Content(List<Paragraph> paragraphs) {
     public record Paragraph(String content, List<String> uris) {}
   }
